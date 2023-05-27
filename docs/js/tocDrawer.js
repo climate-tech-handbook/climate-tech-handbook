03b9b0f57f339c25bac81df1fd490a9fa2d7e4d4
bigolboyyo <wesley.grant94@gmail.com>
2023-05-27 18:46:59 -0400

    abstract components

--- docs/js/tocDrawer.js
+++ docs/js/tocDrawer.js
@@ -1,18 +1,18 @@
+// Wait until the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', function() {
+    // Get references to the TOC button and the TOC drawer
     var tocButton = document.querySelector('.md-header__button[for="__toc"]');
     var toc = document.querySelector('#__toc');
-    var overlay = document.querySelector('.md-overlay'); // Use the existing overlay
-
-    function toggleDrawer() {
-        if (toc.classList.contains('md-drawer--open')) {
-            toc.classList.remove('md-drawer--open');
-            overlay.classList.remove('md-overlay--active');
-        } else {
-            toc.classList.add('md-drawer--open');
-            overlay.classList.add('md-overlay--active');
-        }
-    }
     
-    tocButton.addEventListener('click', toggleDrawer);
-    overlay.addEventListener('click', toggleDrawer);
+    // When the TOC button is clicked...
+    tocButton.addEventListener('click', function() {
+        // If the TOC drawer is currently open...
+        if (toc.classList.contains('md-drawer--open')) {
+            // Close it by removing the 'md-drawer--open' class
+            toc.classList.remove('md-drawer--open');
+        } else {
+            // Otherwise, open it by adding the 'md-drawer--open' class
+            toc.classList.add('md-drawer--open');
+        }
+    });
 });
