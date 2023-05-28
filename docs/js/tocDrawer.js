a00bd642f75003f80e615d6bda2d8f88318e6612
bigolboyyo <wesley.grant94@gmail.com>
2023-05-28 16:35:09 -0400

    making progress | needs more conditional rendering fine tuning

--- docs/js/tocDrawer.js
+++ docs/js/tocDrawer.js
@@ -1,18 +1,28 @@
-// Wait until the DOM is fully loaded
-document.addEventListener('DOMContentLoaded', function() {
+// // Wait until the DOM is fully loaded
+// document.addEventListener('DOMContentLoaded', function() {
 
-    // Get references to the TOC button and the TOC drawer
-    const tocButton = document.querySelector('.md-header__button[for="__toc"]');
-    const toc = document.querySelector('#__toc');
-    // When the TOC button is clicked...
+//     // Get references to the TOC button and the TOC drawer
+//     const tocButton = document.querySelector('.md-header__button[for="__toc"]');
+//     const toc = document.querySelector('#__toc');
+//     // When the TOC button is clicked...
+//     tocButton.addEventListener('click', function() {
+//         // If the TOC drawer is currently open...
+//         if (toc.classList.contains('md-drawer--open')) {
+//             // Close it by removing the 'md-drawer--open' class
+//             toc.classList.remove('md-drawer--open');
+//         } else {
+//             // Otherwise, open it by adding the 'md-drawer--open' class
+//             toc.classList.add('md-drawer--open');
+//         }
+//     });
+// });
+
+// js/tocDrawer.js
+document.addEventListener('DOMContentLoaded', function() {
+    const tocButton = document.getElementById('toc-button');
+    const tocDrawer = document.getElementById('toc-drawer');
+
     tocButton.addEventListener('click', function() {
-        // If the TOC drawer is currently open...
-        if (toc.classList.contains('md-drawer--open')) {
-            // Close it by removing the 'md-drawer--open' class
-            toc.classList.remove('md-drawer--open');
-        } else {
-            // Otherwise, open it by adding the 'md-drawer--open' class
-            toc.classList.add('md-drawer--open');
-        }
+        tocDrawer.classList.toggle('md-drawer--open');
     });
 });
