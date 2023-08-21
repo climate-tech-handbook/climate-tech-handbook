bca7a497a94f8fe5084b9ee0f1c181c458d9bfef
Saki <sbasken@gmail.com>
2023-08-21 12:17:17 -0700

    Truncate image card

--- src/components/ImageCard/ImageCard.tsx
+++ src/components/ImageCard/ImageCard.tsx
@@ -23,14 +23,27 @@ import { useHistory } from 'react-router-dom';
 
     const handleClick = () => {
         history.push(linkUrl);
-      };
+    };
+
+    const characterLimit = 80;
+    let truncatedDescription = description;
+    if (truncatedDescription.length > characterLimit) {
+        // Find the last space within the character limit
+        const lastSpaceIndex = truncatedDescription.lastIndexOf(' ', characterLimit);
+        if (lastSpaceIndex !== -1) {
+            truncatedDescription = truncatedDescription.substring(0, lastSpaceIndex) + '...';
+        } else {
+            truncatedDescription = truncatedDescription.substring(0, characterLimit) + '...';
+        }
+    }
+
 
     return (
         <div className={clsx("cardContainer", styles.cardContainer)} onClick={handleClick}>
             <img className={clsx("img", styles.cardImg)} src={imageUrl} alt={title} />
             <div className={clsx("content", styles.content)}>
                 <h2>{title}</h2>
-                <p>{description}
+                <p>{truncatedDescription}
                     <Link
                         to={linkUrl}
                         className={clsx(
