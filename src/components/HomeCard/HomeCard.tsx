7a5fed1a269fbd3c9a4f789f258fa0854361e906
Michele Zhang <michelezhang98@gmail.com>
2023-12-21 15:30:36 -0500

    update homeCard css

--- src/components/HomeCard/HomeCard.tsx
+++ src/components/HomeCard/HomeCard.tsx
@@ -6,30 +6,29 @@ import styles from "./HomeCard.module.css";
 interface CardProps {
   title: string;
   description: string;
-  imageUrl: string;
+  // imageUrl: string;
   linkUrl: string;
 }
 
 const HomeCard: React.FC<CardProps> = ({
   title,
   description,
-  imageUrl,
+  // imageUrl,
   linkUrl,
 }) => {
   return (
-    <div className={clsx("container", styles.cardContainer)}>
-      <img className={clsx(styles.cardImg)} src={imageUrl} alt={title} />
-      <div className={clsx(styles.cardText)}>
-        <h1>{title}</h1>
-        <p className={clsx(styles.cardDescription)}>{description}</p>
-        <Link
-          to={linkUrl}
-          className={styles.buttonMain}
-        >
-          Get Started
-        </Link>
+    <Link
+      to={linkUrl}
+      className={styles.buttonMain}
+    >
+      <div className={clsx("container", styles.cardContainer)}>
+        {/* <img className={clsx(styles.cardImg)} src={imageUrl} alt={title} /> */}
+        <div className={clsx(styles.cardText)}>
+          <h1>{title}</h1>
+          {/* <p className={clsx(styles.cardDescription)}>{description}</p> */}
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -77,45 +77,46 @@ export default function Home(): JSX.Element {
       <main className={clsx(styles.mainBody)}>
         <NewClimateClock countDown={climateCountdown} />
 
-       <HomeCard
-          title="About Us"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat 
-            nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-            sunt in culpa qui officia deserunt mollit anim id est laborum."
-          imageUrl=""
-          linkUrl="/about"
-        />
-        
-       <HomeCard
-          title="Courses"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat 
-            nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-            sunt in culpa qui officia deserunt mollit anim id est laborum."
-          imageUrl=""
-          linkUrl="/intro"
-        />
+        <div className={clsx(styles.homecardContainer)}>
+          <HomeCard
+              title="About Us"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+                sunt in culpa qui officia deserunt mollit anim id est laborum."
+              // imageUrl="/img/main/cth-earth-logo.png"
+              linkUrl="/about"
+          />
+            
+          <HomeCard
+              title="Courses"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+                sunt in culpa qui officia deserunt mollit anim id est laborum."
+              // imageUrl="/img/main/cth-earth-logo.png"
+              linkUrl="/intro"
+          />
 
-      <HomeCard
-          title="Climate Solutions"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat 
-            nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-            sunt in culpa qui officia deserunt mollit anim id est laborum."
-          imageUrl=""
-          linkUrl="/solutions"
-          // imageUrl={useBaseUrl("/img/climate-startups.webp")}
-        />
+          <HomeCard
+              title="Climate Solutions"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+                sunt in culpa qui officia deserunt mollit anim id est laborum."
+              // imageUrl="/img/main/cth-earth-logo.png"
+              linkUrl="/solutions"
+          />
+        </div>
       </main>
 
     </Layout>
