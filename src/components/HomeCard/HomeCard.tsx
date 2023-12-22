375ee328eca9dbb0dc9f46488182361d14d81e42
Michele Zhang <michelezhang98@gmail.com>
2023-12-22 15:16:47 -0500

    remove months from climateClock, add icons to homecard component

--- src/components/HomeCard/HomeCard.tsx
+++ src/components/HomeCard/HomeCard.tsx
@@ -3,27 +3,50 @@ import React, { CSSProperties } from "react";
 import Link from "@docusaurus/Link";
 import styles from "./HomeCard.module.css";
 
+import { IoEarth } from "react-icons/io5";
+import { FaBookOpen } from "react-icons/fa";
+import { MdWindPower } from "react-icons/md";
+
 interface CardProps {
   title: string;
   description: string;
-  // imageUrl: string;
   linkUrl: string;
+  icon: string;
 }
 
 const HomeCard: React.FC<CardProps> = ({
   title,
   description,
-  // imageUrl,
   linkUrl,
+  icon
 }) => {
+
+  const iconComponent = () => {
+    switch(icon) {
+      case 'IoEarth': {
+        return <IoEarth />;
+      }
+      case 'FaBookOpen': {
+        return <FaBookOpen />;
+      }
+      case 'MdWindPower': {
+        return <MdWindPower />;
+      }
+      default: {
+        return null;
+      }
+    }
+    
+  }
+
   return (
     <Link
       to={linkUrl}
       className={styles.cardLink}
     >
       <div className={clsx("container", styles.cardContainer)}>
-        {/* <img className={clsx(styles.cardImg)} src={imageUrl} alt={title} /> */}
         <div className={clsx(styles.cardText)}>
+          {iconComponent()}
           <h1>{title}</h1>
           <p className={clsx(styles.cardDescription)}>
             {description}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,17 @@
 import React, { useEffect, useState } from "react";
+import clsx from "clsx";
 import moment from "moment";
 import Layout from "@theme/Layout";
+import styles from "./index.module.css";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import HomepageHeader from "../components/HomepageHeader/HomepageHeader";
 import HomeCard from "../components/HomeCard/HomeCard";
-import styles from "./index.module.css";
-import clsx from "clsx";
 import NewClimateClock from "../components/NewClimateClock/NewClimateClock";
-import useBaseUrl from "@docusaurus/useBaseUrl";
 
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
-
   const [climateCountdown, setClimateCountdown] = useState({
     years: 0,
-    // months: 0,
     days: 0,
     hours: 0,
     minutes: 0,
@@ -44,20 +41,6 @@ export default function Home(): JSX.Element {
       });
   }, 1000);
 
-  // With Months included in climateClock
-  // useEffect(() => {
-  //   const timeBetween = moment.duration(dateOfDeadline.diff(moment()));
-  //   const interval = setInterval(() => {
-  //     setClimateCountdown({
-  //       years: timeBetween.years(),
-  //       months: timeBetween.months(),
-  //       days: timeBetween.days(),
-  //       hours: timeBetween.hours(),
-  //       minutes: timeBetween.minutes(),
-  //       seconds: padWithZeroes(timeBetween.seconds(), 2)
-  //     });
-  //   }, 1000);
-
     return () => clearInterval(interval);
   }, [climateCountdown]);
 
@@ -77,29 +60,42 @@ export default function Home(): JSX.Element {
       <main className={clsx(styles.mainBody)}>
         <NewClimateClock countDown={climateCountdown} />
 
+        <div className={clsx(styles.infoDiv)}>
+          <h1>OUR MISSION</h1>
+
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
+            commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
+            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim 
+            id est laborum.
+          </p>
+        </div>
+
         <div className={clsx(styles.homecardContainer)}>
           <HomeCard
               title="About Us"
               description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
               sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-              // imageUrl="/img/main/cth-earth-logo.png"
               linkUrl="/about"
+              icon="IoEarth"
           />
             
           <HomeCard
               title="Courses"
               description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
               sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-              // imageUrl="/img/main/cth-earth-logo.png"
               linkUrl="/intro"
+              icon="FaBookOpen"
           />
 
           <HomeCard
               title="Climate Solutions"
               description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
               sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-              // imageUrl="/img/main/cth-earth-logo.png"
               linkUrl="/solutions"
+              icon="MdWindPower"
           />
         </div>
       </main>
