a098f5f62071a40dedd6ff0bb61a0e8cc14016bc
Michele Zhang <michelezhang98@gmail.com>
2024-01-03 16:02:28 -0500

    styling clock to fit all details

--- src/components/NewClimateClock/NewClimateClock.jsx
+++ src/components/NewClimateClock/NewClimateClock.jsx
@@ -16,13 +16,6 @@ export default function NewClimateClock({ countDown }) {
             <div className={clsx(styles.label)}>Years</div>
           </div>
 
-          {/* <span> : </span>
-
-          <div className={clsx(styles.section)}>
-            <div className={clsx(styles.number)}>{countDown.months}</div>
-            <div className={clsx(styles.label)}>Mon</div>
-          </div> */}
-
           <span> : </span>
 
           <div className={clsx(styles.section)}>
