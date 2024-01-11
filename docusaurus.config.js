45b42ebb5220d11fe5870a4074bb99d2ea489454
Michele Zhang <michelezhang98@gmail.com>
2024-01-11 01:50:27 -0500

    add more logo and icon spacing

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -123,10 +123,10 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
                     <img src="/img/main/cth-earth-logo.png" alt="CTH Earth Logo">
                     <div class="social-links">
                       <a href="https://chat.climatetechhandbook.com" target="_blank" rel="noreferrer noopener" aria-label="Slack">
-                        <img src="img/slack_icon.png" alt="Slack Icon" style="margin-right: 8px; width: 40px;"/>
+                        <img src="img/slack_icon.png" alt="Slack Icon" style="margin-right: 15px; width: 40px;"/>
                       </a>
                       <a href="https://www.linkedin.com/company/climate-tech-handbook/" target="_blank" rel="noreferrer noopener" aria-label="LinkedIn">
-                        <img src="img/design/icons/linkedin_icon.svg" alt="LinkedIn Icon" style="margin-right: 8px;"/>
+                        <img src="img/design/icons/linkedin_icon.svg" alt="LinkedIn Icon" style="margin-right: 15px;"/>
                       </a>
                       <a href="https://github.com/climate-tech-handbook" target="_blank" rel="noreferrer noopener" aria-label="Github">
                         <img src="img/design/icons/github-mark-black.svg" alt="Github Icon" style="width: 40px;" />
