12b6c4a8ffaed9d3cf05a0f28c4e38126b2d5323
Matt McBurnett <mattmcburnett@gmail.com>
2024-06-03 14:58:09 -0700

    updateTags function

--- src/components/SolutionFilter/SolutionFilter.tsx
+++ src/components/SolutionFilter/SolutionFilter.tsx
@@ -1,16 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
 import ImageCard from "../ImageCard/ImageCard";
 import { Chip } from "@mui/material";
 
+
 const SolutionFilter: React.FC<{ solutions: any[] }> = ({ solutions }) => {
 
-    const tags: string[] = []
+    const uniqueTags: string[] = [];
+    const selectedTags = new Set<string>();
 
     const findTags = () => {
-        
-    }
+        solutions.forEach((solution) => {
+            solution.tags.forEach((tag: string) => {
+                if (!uniqueTags.includes(tag)) {
+                    uniqueTags.push(tag);
+                }
+            });
+        });
+    };
+
+    //TODO - update tags function to add or remove tags from the selectedTags set - WORKS
+    const updateTags = (tag) => {
+        if (selectedTags.has(tag)) {
+            selectedTags.delete(tag);
+        } else {
+            selectedTags.add(tag);
+        }
+    };
+
+    const renderFilterChips = () => {
+        findTags();
+
+        return uniqueTags.map((tag) => (
+            <Chip
+                label={tag}
+                onClick={() => {
+                    updateTags(tag);
+                }}
+            />
+        ));
+    };
 
 
+    //TODO - update grid function to filter solutions based on selected tags
     const renderSolutionGrid = () => {
         return solutions.map((solution) => (
             <ImageCard
@@ -22,7 +53,16 @@ const SolutionFilter: React.FC<{ solutions: any[] }> = ({ solutions }) => {
         ));
     };
 
-    return <div className="solution-grid">{renderSolutionGrid()}</div>;
+    return (
+        <div>
+            <div>
+                {renderFilterChips()}
+            </div>
+            <div className="solution-grid" style={{display:"flex", flexWrap:"wrap"}}>
+            {renderSolutionGrid()}</div>
+        </div>
+
+        )
 };
 
 
