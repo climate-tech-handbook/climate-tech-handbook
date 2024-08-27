644dc8a1e7b4306875fb0488dc19eb0b1aef8b39
Brett Baker <GitBaBB@MyBaBB.com>
2024-08-27 07:35:37 -0700

    the deploy preview will be at 531 on the next netlify so I changed it to that on the image URL,   I dont know what happened to our deploy branch.  It would be easier on that branch.  Ok testing

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -45,14 +45,14 @@ const darkCodeTheme = themes.dracula;
       tagName: 'meta',
       attributes: {
         property: 'og:image',
-        content: 'https://picsum.photos/200/300 ',
+        content: 'https://deploy-preview-531--ct-handbook-public.netlify.app/ ',
       },
     },
     {
       tagName: 'meta',
       attributes: {
         property: 'og:url',
-        content: 'https://picsum.photos/200/300',
+        content: 'https://deploy-preview-531--ct-handbook-public.netlify.app/',
       },
     },
   ],
